be202125e2bc3053e616bf47624120f622454c19
ki5n2 <106531096+ki5n2@users.noreply.github.com>
2022-07-11 04:32:42 +0000

    Deploying to gh-pages from  @ bff2a808cf60a0a949f312c8a4d30c4029333a55 🚀

---
+++ index.jl
@@ -0,0 +1,79 @@
+### A Pluto.jl notebook ###
+# v0.19.4
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 9ba3fd28-a02f-11ec-2d00-417ff6fef7d3
+md"""
+# 줄리아 학습 블로그
+"""
+
+# ╔═╡ 2cfc8784-d7cf-4ab7-8934-855fecad2b26
+md"""
+### Info
+
+- author:
+- lecture:
+- ...
+
+"""
+
+# ╔═╡ f589bb03-2bf2-4f65-b351-0e3c53eae11b
+md"""
+### Posts
+"""
+
+# ╔═╡ 1f7b4001-be12-44dc-8745-c45ed7d2ad03
+md"""
+[Statistic Computing 9](https://ki5n2.github.io/forestgreen/2022-04-14-SC.html)
+- 정규분포, delta method
+
+[Statistic Computing 8](https://ki5n2.github.io/forestgreen/2022-04-12-SC.html)
+- 정규분포
+
+[Statistic Computing 7](https://ki5n2.github.io/forestgreen/2022-04-11-SC.html)
+- 어느 사격수 이야기(2), 정규분포
+
+[Statistic Computing 6](https://ki5n2.github.io/forestgreen/2022-04-08-SC.html)
+- Inverse cdf의 이론적 근거, 어느 사격수 이야기(1)
+
+[Statistic Computing 5](https://ki5n2.github.io/forestgreen/2022-04-05-SC.html)
+- Inverse cdf method motive, 지수함수의 무기억성, 몬테카를로 적분, 박스뮬러변환
+
+[Statistic Computing 4](https://ki5n2.github.io/forestgreen/2022-03-28-SC.html)
+- 포아송분포, 지수분포
+
+[Statistic Computing 3](https://ki5n2.github.io/forestgreen/2022-03-25-SC.html)
+- 베르누이분포, 이항분포
+
+[Statistic Computing 2](https://ki5n2.github.io/forestgreen/2022-03-22-SC.html)
+- 추가학습 풀이, 함수고급
+
+[Statistic Computing 1](https://ki5n2.github.io/forestgreen/2022-03-21-SC.html)
+- 매트릭스, 벡터와 매트릭스, 인덱싱, 함수선언
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─9ba3fd28-a02f-11ec-2d00-417ff6fef7d3
+# ╟─2cfc8784-d7cf-4ab7-8934-855fecad2b26
+# ╟─02d4f860-29eb-4fd9-bbc5-4baeb819560d
+# ╟─f589bb03-2bf2-4f65-b351-0e3c53eae11b
+# ╟─1f7b4001-be12-44dc-8745-c45ed7d2ad03
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
